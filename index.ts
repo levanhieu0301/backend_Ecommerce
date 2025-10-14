ab042605b457bc25f399ff0866af17b028ec38e7
lvh0301 <levanhieu0301@gmail.com>
2025-10-14 13:40:29 +0700

    Ghép code font-end client vào giao diện

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
 import express, {Request, Response} from "express";
-
+import path from "path";
 const app = express()
 const port = 5000
 
+// Tích hợp pug cho dự án
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+// Tích hợp file tĩnh 
+app.use(express.static(path.join(__dirname, "public")))
+
 app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
+  res.render('client/pages/home', {
+    pageTitle: "Trang chủ"
+  })
 })
 
 app.listen(port, () => {
